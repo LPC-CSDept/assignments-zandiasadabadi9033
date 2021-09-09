c94b2abd1f64d5a8b605ad5d527404ff3dd1ed6b
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-09 19:18:11 +0000

    Final

--- Assignment 1-1/readFile2.cpp
+++ Assignment 1-1/readFile2.cpp
@@ -24,17 +24,44 @@ int main() {
 	ifs.read( (char *)&s, sizeof(s) );
 
   // Shows the student ID's so that the user can pick one
-  cout << "The Student ID's are:\n";
-  for (int i = 0; i < SIZE; i++) {
-	  cout << s[i].ID << endl;
-  }
+  char sentinel;
+  do {
+    cout << "\nEnter 'A' to see all of the students' data\n";
+    cout << "Enter 'B' to see all the IDs and then pick one student ID to visit the data\n";
+    cout << "Enter 'Q' to quit\n";
+    cout << "Enter your command here: ";
+    cin >> sentinel;
 
-  // Asks the user to pick an ID that would allow them to see the student's data
-  cout << "\nEnter the ID of the student that you would like to get to know more about: ";
-  cin >> numID;
+    if (toupper(sentinel) == 'A') {
+      for (int i = 0; i < 10; i++) {
+		    cout << s[i].ID << "\t" ;
+		    cout << s[i].name << "\t";
+		    cout << s[i].score[0] << " " << s[i].score[1] << "\t";
+		    cout << s[i].sum << " " << s[i].avg << endl;
+	    }
+    }
 
-  // Calls the function that would check to see if the entered value matches one of the student ID's, and if so, returns the data for that particular student
-  linearSearch(s, SIZE, numID);
+    else if (toupper(sentinel) == 'B') {
+      cout << "The Student ID's are:\n";
+      for (int i = 0; i < SIZE; i++) {
+	      cout << s[i].ID << endl;
+      }
+
+      // Asks the user to pick an ID that would allow them to see the student's data
+      cout << "\nEnter the ID of the student that you would like to get to know more about: ";
+      cin >> numID;
+
+      // Calls the function that would check to see if the entered value matches one of the student ID's, and if so, returns the data for that particular student
+      linearSearch(s, SIZE, numID);
+    }
+    else if (toupper(sentinel) == 'Q') {
+      cout << "\nProgram is terminated\n";
+    }
+
+    else {
+      cout << "\nYour input is invalid... Try again...\n";
+    }
+  } while (toupper(sentinel) != 'Q');
 
   return 0;
 }
@@ -43,7 +70,7 @@ int main() {
 void linearSearch(Student s[], int n, int target) {
   for (int i = 0; i < n; i++) {
     if (s[i].ID == target) {
-      cout << "ID: " << s[i].ID << "\t";
+      cout << "\nID: " << s[i].ID << "\t";
 	    cout << "Name: " << s[i].name << "\t";
 	    cout << "Score #1: " << s[i].score[0] << "\t";
 	    cout << "Score #2: " << s[i].score[1] << "\t";
