d3627e6e7b46ea1ce5bfded35cd23d2965465f2d
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-10 02:40:18 +0000

    demo

--- Assignment 8-1/binsearch.cpp
+++ Assignment 8-1/binsearch.cpp
@@ -2,12 +2,35 @@
 #include <algorithm> // needed to use the sort function.
 using namespace std;
 
+
+int binarySearch(int [], int, int);
+// 1st parameter : Integer Array
+// 2nd Parameter : Size of the array N
+// 3rd Parameter : Taget Value
+// Return value and type : the index of the array if the target is in the array. -1 if not found.
+
+
 int main()
 {
-	const int N = 10;
-	int numbers[N] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
+	const int SIZE = 10;
+	int numbers[SIZE] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
 	int target = 35;
+	int index;
 
 
+	index = binarySearch(numbers, SIZE, target );
+	if ( index == -1)
+		cout << "Not found\n";
+	else
+		cout << "Found at index " << index << endl;
+}
 
+int binarySearch(int number[], int N, int target)
+{
+	int first, last, mid;
+	first = 0;
+  last = N - 1;
+    
+
+	return -1;
 }
