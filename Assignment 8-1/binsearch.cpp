2c5f52c1365b8d33c7fd98cf38ee1b14129b9f20
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-14 21:31:01 +0000

    Fin

--- Assignment 8-1/binsearch.cpp
+++ Assignment 8-1/binsearch.cpp
@@ -1,8 +1,5 @@
-// NOT FINISHED
-
-
 #include <iostream>
-#include <algorithm> // needed to use the sort function.
+#include <algorithm> 
 using namespace std;
 
 
@@ -16,22 +13,31 @@ int binarySearch(int [], int, int);
 int main()
 {
   const int SIZE = 10;
-  int numbers[SIZE] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
+  int arr[SIZE] = {10, 25, 30, 35, 40, 45, 55, 60, 65, 70 };
   int target = 35;
-  int index;
   
-  index = binarySearch(numbers, SIZE, target );
-  if ( index == -1)
-    cout << "Not found\n";
-  else
-  cout << "Found at index " << index << endl;
+  binarySearch(arr, SIZE, target );
+
+  return 0;
 }
 
-int binarySearch(int number[], int N, int target)
+int binarySearch(int arr[], int N, int target)
 {
-  int first, last, mid;
-  first = 0;
-  last = N - 1;
+  int first = 0, last = N - 1, mid;
   
+  while (first <= last) {
+    mid = (first + last) / 2;
+    if (arr[mid] == target) {
+      cout << "The target was found at index #" << mid << endl;
+      return mid;
+    }
+    else if (arr[mid] < target) {
+      first = mid + 1;
+    } 
+    else if (arr[mid] > target){
+      last = mid - 1;
+    }
+  }
+
   return -1;
 }
