0a51d0bd57ddbf4cff7808fe97daf4aa582f1133
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-15 02:23:57 +0000

    debugged

--- Assignment 8-3/selectionSort.cpp
+++ Assignment 8-3/selectionSort.cpp
@@ -2,11 +2,43 @@
 
 using namespace std;
 
+int findMin(int [], int, int);
+void printArray(int [], int);
+
 const int SIZE = 10;
 
 int main() {
   
-  int numbers[SIZE] = {25, 10, 15, 30, 35, 40, 45, 55};
+  int numbers[SIZE] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
+  printArray(numbers, SIZE);
   
-  return 0;
+  for(int i = 0; i < SIZE - 1; i++) {
+    int minidx;
+    minidx = findMin(numbers, SIZE, i); //Find the i_th smallest number
+    swap(numbers[i], numbers[minidx]);
+    }
+    printArray(numbers, SIZE);
+
+}
+
+int findMin(int arr[], int N, int ith) {
+
+  int min, minidx;
+  min = arr[ith];
+  for (int j = ith; j < SIZE; j++) {
+    if (min > arr[j]) {
+      min = arr[j];
+      minidx = j;
+    }
+  }
+  return minidx;
+}
+
+void printArray(int arr[], int N) {
+
+  for(int i = 0; i < N; i++){
+    cout << arr[i] << "\t";
+    }
+    cout << endl;
+
 }
