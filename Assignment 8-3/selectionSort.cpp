0237a6eead0e47c40afcb12c996f464ebbe382cd
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-21 04:05:08 +0000

    Debugged

--- Assignment 8-3/selectionSort.cpp
+++ Assignment 8-3/selectionSort.cpp
@@ -53,12 +53,15 @@ void printArray(int arr[], int N) {
 
 void selectionSort(int arr[], int N) {
 
-  int min, minidx;
+  int minidx;
   for (int i = 0; i < N - 1; i++) {
-    for (int j = 0; j < N - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        swap(arr[j], arr[j + 1]);
+    minidx = i;
+    for (int j = i + 1; j < N; j++) {
+      if (arr[j] < arr[minidx]) {
+        minidx = j;
       }
+     
     }
+    swap(arr[minidx], arr[i]);
   }
 }
