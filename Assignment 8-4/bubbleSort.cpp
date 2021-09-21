b72d743f246cc836044e01317976afa9954fe0f3
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-09-21 02:25:06 +0000

    demo

--- Assignment 8-4/bubbleSort.cpp
+++ Assignment 8-4/bubbleSort.cpp
@@ -1,34 +1,116 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-void printArray(int [], int);
+const int lengthSIZE = 20;
+const int scoreSIZE = 3;
+
+struct Students {
+
+  int ID;
+  char name[lengthSIZE];
+  double scores[scoreSIZE];
+
+};
+
+void readFile(Students [], int);
+void bubbleSortbyID(Students [], int);
+void bubbleSortbyName(Students[], int);
+void bubbleSortbyScores(Students[], int);
+void printArray(Students [], int);
 
 int main() {
   
   const int SIZE = 10;
-  int numbers[SIZE] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
+  Students student[SIZE];
+
+  readFile(student, SIZE);
+  cout << "The Student struct array is:" << endl;
+  printArray(student, SIZE);
   
-  cout << "The unsorted array is:" << endl;
-  printArray(numbers, SIZE);
+  cout << "\nThe sorted Student struct array by ID is:" << endl;
+  bubbleSortbyID(student, SIZE);
+  printArray(student, SIZE);
   
-  for (int i = 0; i < SIZE - 1; i++){
-    for(int j = 0; j < SIZE - 1; j++) {
-      if(numbers[j] > numbers[j + 1])
-      swap(numbers[j], numbers[j + 1]);
-    }
-  }
+  /*cout << "\nThe sorted Student struct array by Name is:" << endl;
+  bubbleSortbyName(student, SIZE);
+  printArray(student, SIZE);
   
-  cout << "\nThe sorted array using Bubble Sort is:" << endl;
-  printArray(numbers, SIZE);
+  cout << "\nThe sorted Student struct array by Scores are:" << endl;
+  bubbleSortbyScores(student, SIZE);
+  printArray(student, SIZE);*/
 
   return 0;
 }
 
-void printArray(int arr[], int N) {
-
-  for(int i = 0; i < N; i++) {
-    cout << arr[i] << "\t";
+void readFile(Students arr[], int N) {
+  
+  ifstream ifs;
+  ifs.open("students.txt");
+  
+  if (ifs.fail()) {
+    cerr << "File could not be opened...\n";
+    exit(0);
+  }
+  
+  for (int i = 0; i < N; i++) {
+    ifs >> arr[i].ID >> arr[i].name;
+    
+    for (int j = 0; j < scoreSIZE; j++) {
+      ifs >> arr[i].scores[j];
     }
+    
+    if (ifs.fail()) {
+      cerr << "File could not be read...\n";
+      exit(0);
+    }
+  }
 
+}
+
+void bubbleSortbyID(Students arr[], int N) {
+  
+  int counter = 0;
+  bool sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (int i = N - 1; i > counter; i--) {
+      if (arr[i].ID < arr[i - 1].ID) {
+        swap(arr[i], arr[i - 1]);
+        sorted = false;
+      }
+    }
+    counter++;
+  }
+}
+  /*for (int i = 0; i < N - 1; i++) {
+    for(int j = 0; j < N - 1; j++) {
+      if (arr[j].ID > arr[j + 1].ID) {
+        swap(arr[j].ID, arr[j + 1].ID);
+        swap(arr[j].name, arr[j + 1].name);
+        swap(arr[j].scores[i], arr[j + 1].scores[i + 1]);
+      }
+    }
+  }*/
+
+void bubbleSortbyName(Students arr[], int N) {
+
+}
+
+
+void bubbleSortbyScores(Students arr[], int N) {
+
+}
+
+void printArray(Students arr[], int N) {
+  
+  for(int i = 0; i < N; i++) {
+    cout << arr[i].ID << "\t" << arr[i].name << "\t";
+    for (int j = 0; j < scoreSIZE; j++) {
+      cout << arr[i].scores[j] << "\t";
+    }
+  cout << endl;
+  }
+      
   cout << endl;
 }
