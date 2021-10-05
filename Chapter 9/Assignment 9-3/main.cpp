c3ed2071dd7a58bd4a055375cf509089f2b250c1
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-10-05 23:21:50 +0000

    debugging..

---
+++ Chapter 9/Assignment 9-3/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+double *makeNumbers(int);
+void printNumbers(double * const, int);
+void sortNumbers(double * const, int);
+void deleteNumbers(double *);
+
+int main() {
+
+  const int SIZE = 10;
+  double *ptr = nullptr;
+  
+  cout << "The initial Double Values are:\n";
+  ptr = makeNumbers(SIZE);
+	printNumbers(ptr, SIZE);
+
+  cout << "The sorted Double Values are:\n";
+  sortNumbers(ptr, SIZE);
+  printNumbers(ptr, SIZE);
+
+  deleteNumbers(ptr);
+
+  return 0;
+
+}
+
+double *makeNumbers(int N) {
+
+  srand(time(NULL));
+  double *temp = new double[N];
+  for (int i = 0; i < N; i++) {
+    temp[i] = rand() % 100 + 1;
+  }
+  return temp;
+
+}
+
+void printNumbers(double * const ptr, int N) {
+
+  for (int i = 0; i < N; i++) {
+    cout << *(ptr + i) << " ";
+  }
+  
+  cout << endl;
+
+}
+
+void sortNumbers(double * const ptr, int N) {
+  
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N - i - 1; j++) {
+      if (ptr[j] > ptr[j + 1]) {
+        swap(ptr[j], ptr[j + 1]);
+      }
+    }
+  }
+
+}
+
+void deleteNumbers(double * ptr) {
+  
+  delete [] ptr;
+
+}
