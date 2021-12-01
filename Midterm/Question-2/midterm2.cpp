5036f79e90c195b86be2cdff6774e05558f95703
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-01 06:49:42 +0000

    main skeleton

---
+++ Midterm/Question-2/midterm2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Student.hpp"
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  const int SIZE = 10;
+  Student students[SIZE];
+  int value;
+
+  // makeStudents(students, SIZE);
+  // sort by descending scores
+  // printStudents(students, SIZE);
+
+  cout << "Please enter the ID of the student that you are looking for: ";
+  cin >> value;
+
+  // binarySearch to find the value
+  // print out
+
+
+
+
+  return 0;
+
+}
