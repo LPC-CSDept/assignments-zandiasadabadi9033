edf9cd17cb6f879ab561bb75d33673d4bf55f751
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-16 23:24:44 +0000

    main.cpp skeleton created

---
+++ Final/Q1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include "Course.hpp"
+
+using namespace std;
+
+int main() {
+
+  int SIZE = 10;
+  Course courses[SIZE];
+
+  fstream ifs;
+  ifs.open("textFile.txt");
+
+  for (int i = 0; i < SIZE; i++) {
+    int ID;
+    string name;
+    int credit;
+    ifs >> ID >> name >> credit;
+    courses[i].setCourse(ID, name, credit);
+  }
+
+  ifs.close();
+
+  return 0;
+
+}
