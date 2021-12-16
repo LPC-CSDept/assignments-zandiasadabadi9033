8d8bb633324ea412627aa6d1cbb0cad59e19b5ce
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-16 23:30:55 +0000

    separated files...

---
+++ Final/Q1/Course.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "Course.h"
+
+using namespace std;
+
+Course::Course() {
+  
+  ID = 0;
+  name = "";
+  credit = 0;
+  
+}
+
+void Course::setCourse(int id, string cname, int creditNum) {
+  
+  ID = id;
+  name = cname;
+  credit = creditNum;
+  
+}
+
+int Course::getID() {
+  
+  return ID;
+  
+}
+
+string Course::getName() {
+  
+  return name;
+  
+}
+
+int Course::getCredit() {
+  
+  return credit;
+  
+}
--- Final/Q1/main.cpp
+++ Final/Q1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include "Course.hpp"
+#include "Course.h"
 
 using namespace std;
 
