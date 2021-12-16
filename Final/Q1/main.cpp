b377247fc788611a12f57d0bc481633fe895df2e
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-16 23:38:28 +0000

    first binarySearch completed...

--- Final/Q1/main.cpp
+++ Final/Q1/main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+int binary_search(Course, int, int);
+int recursive_binary_search(Course, int, int, int);
+
 int main() {
 
   int SIZE = 10;
@@ -23,5 +26,33 @@ int main() {
   ifs.close();
 
   return 0;
+
+}
+
+int binary_search(Course arr[], int N, int num) {
+
+  int first = 0, last = N - 1;
+
+  while (first <= last) {
+
+    int middle = (first + last) / 2;
+    if (arr[middle].getID() == num) {
+      return middle;
+    }
+    if (arr[middle].getID() < num) {
+      first = middle + 1;
+    }
+    if (arr[middle].getID() > num){
+      last = middle - 1;
+    }
+    
+  }
+
+  return -1;
+
+}
+
+int recursive_binary_search(Course arr[], int left, int right, int num) {
+
 
 }
