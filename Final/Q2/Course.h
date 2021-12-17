8f12c733523d95bae47c485e1da2ccd13026a194
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-17 00:06:13 +0000

    copying the Course class from Q1 to Q2

---
+++ Final/Q2/Course.h
@@ -0,0 +1,23 @@
+#ifndef COURSE_H
+#define COURSE_H
+#include <iostream>
+
+using namespace std;
+
+class Course {
+
+private:
+  int ID;
+  string name;
+  int credit;
+public:
+  Course();
+  void setCourse(int, string, int);
+  int getID();
+  string getName();
+  int getCredit();
+  void printCourse();
+  
+};
+
+#endif
