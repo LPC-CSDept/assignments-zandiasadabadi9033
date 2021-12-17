55db17f88ff8a78a38de8342b03332ed09b11d0b
szclassic <32965641+zandiasadabadi9033@users.noreply.github.com>
2021-12-17 00:12:30 +0000

    copied the main skeleton from Q1 to Q2

---
+++ Final/Q2/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include "Course.h"
+
+using namespace std;
+
+void bubbleSort(Course [], int);
+void print(Course [], int);
+
+int main() {
+  
+  int SIZE = 10;
+  Course courses[SIZE];
+  
+  fstream ifs;
+  ifs.open("textFile.txt");
+  
+  for (int i = 0; i < SIZE; i++) {
+    int ID;
+    string name;
+    int credit;
+    ifs >> ID >> name >> credit;
+    courses[i].setCourse(ID, name, credit);
+  }
+  
+  cout << "The sorted courses are: " << endl;
+  bubbleSort(courses, SIZE);
+  print(courses, SIZE);
+  
+  int ans1, ans2;
+  cout << "\nPlease enter an ID value to search: ";
+  cin >> ans1;
+  
+  ifs.close();
+  
+  return 0;
+  
+}
+
+void bubbleSort(Course arr[], int N) {
+  
+  for (int i = 0; i < N - 1; i++) {
+    for (int j = 0; j < N - i - 1; j++) {
+      if (arr[j].getID() > arr[j + 1].getID()) {
+        swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
+  
+}
+
+void print(Course arr[], int N) {
+  
+  for (int i = 0; i < N; i++) {
+    arr[i].printCourse();
+  }
+  
+}
